a5b3dafaa32c4c6b2128cda905612dd16593c38a
AlitaMcF <ai.xufang@qq.com>
2020-05-31 15:01:25 +0800

    update footer

--- footer.php
+++ footer.php
@@ -1,7 +1,33 @@
 <div id="footer">
-    <p id="footer-paragraph">
-        Copyright &#169;
-        <?php echo date('Y'); ?>
-        <?php bloginfo('name'); ?>
-    </p>
+    <div id="footer-maincontent">
+        <div id="footer-social">
+            <div id="footer-social-img-div">
+                <img src="https://shaunfong.com/wp-content/uploads/2020/05/WechatIMG105.jpeg" alt="LongMarch-5" />
+            </div>
+            <div class="footer-social-item">
+                <p class="footer-social-item-head">Some awesome sites I want to share with you</p>
+                <hr id="footer-social-hr" />
+                <p><a href="https://www.coursera.org/">Coursera</a></p>
+                <p><a href="https://www.nasa.gov/">NASA</a></p>
+                <p><a href="https://www.bilibili.com/">Bilibili</a></p>
+            </div>
+
+        </div>
+        <div id="footer-copyright">
+            <p id="footer-paragraph">
+                Copyright &#169;
+                <?php echo date('Y'); ?>
+                <?php bloginfo('name'); ?>
+            </p>
+            <p>
+                <a href="http://www.beian.miit.gov.cn/state/outPortal/loginPortal.action">
+                    粤ICP备19128139号
+                </a>
+            </p>
+        </div>
+    </div>
+    <div id="footer-slogan">
+        <p>CODE IS POETRY, CODE IS SOMETHING BEAUTIFUL</p>
+    </div>
+
 </div>
