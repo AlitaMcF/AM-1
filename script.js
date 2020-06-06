456411064e3ec34b4a06bbcb9a46ffbfd7d96fd9
AlitaMcF <ai.xufang@qq.com>
2020-06-06 17:39:21 +0800

    add code highlight and update some styles

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ jQuery(document).ready(function ($) {
         /*设置goTop按钮的自动消失与出现*/
         var scroHei = $(window).scrollTop(); //滚动的高度
         if (scroHei > 400) {
-            $("#goTop").slideDown(300, function(){});
+            $("#goTop").slideDown(300, function () {});
         } else {
             // slideUp() function will change the height into 0, so button will disappear.
-            $("#goTop").slideUp(300, function(){});
+            $("#goTop").slideUp(300, function () {});
         }
     });
 
@@ -21,26 +21,27 @@ jQuery(document).ready(function ($) {
     });
 });
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     // get the pull-down target
-    var pdlist = $('#header-navigator>ul>li');
+    var pdlist = $("#header-navigator>ul>li");
 
     // add action to mouseenter
-    pdlist.mouseenter(function(){
+    pdlist.mouseenter(function () {
         // $(this).children('ul').stop(true).slideDown(200);
-        $(this).children('ul').stop().show();
+        $(this).children("ul").stop().show();
     });
 
     // add action to mouseleave
-    pdlist.mouseleave(function(){
+    pdlist.mouseleave(function () {
         // $(this).children('ul').stop(true).delay(300).slideUp(200);
-        $(this).children('ul').stop().hide();
+        $(this).children("ul").stop().hide();
     });
 });
 
 // define searchform's click event.
-jQuery(document).ready(function($){
-    $('#searchsubmit').click(function(){
-        $('#searchform').submit();
+jQuery(document).ready(function ($) {
+    $("#searchsubmit").click(function () {
+        $("#searchform").submit();
     });
 });
+
--- single.php
+++ single.php
@@ -4,11 +4,11 @@
 <head>
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
-    <!-- <link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/styles/default.min.css">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/styles/github.min.css">
     <script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/highlight.min.js"></script>
     <script>
         hljs.initHighlightingOnLoad();
-    </script> -->
+    </script>
 </head>
 
 <body id="body-home">
